30a1d3c2ce123b965e8305b6f4ad2280804d8184
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-12-05 01:50:11 +0100

    add available balance text

--- src/App.js
+++ src/App.js
@@ -111,12 +111,13 @@ function App() {
 							<div class="col-xl-4 col-lg-6 col-md-12">
 								<div class="card pull-up ecom-card-1 bg-white">
 									<div class="card-content ecom-card2 height-180">
-										<h5 class="text-muted danger position-absolute p-1">Progress Stats</h5>
+										<h5 class="text-muted danger position-absolute p-1">Available Balance</h5>
 										<div>
 											<i class="ft-pie-chart danger font-large-1 float-right p-1"></i>
 										</div>
 										<div class="progress-stats-container ct-golden-section height-75 position-relative pt-3  ">
 											<div id="progress-stats-bar-chart"></div>
+											<h1 class="available-balance"># 5,600.38</h1>
 											<div id="progress-stats-line-chart" class="progress-stats-shadow"></div>
 										</div>
 									</div>
