0d3d717d1b74c68184575bddab09a7f398eb4209
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-12-05 03:37:35 +0100

    add quick transfer section

--- src/App.js
+++ src/App.js
@@ -312,28 +312,22 @@ function App() {
 											<form class="form">
 												<div class="form-body">
 													<div class="form-group">
-														<label for="donationinput1" class="sr-only">First Name</label>
-														<input type="text" id="donationinput1" class="form-control" placeholder="First Name" name="fname" />
+														<label for="account_number" class="sr-only">First Name</label>
+														<input type="text" id="account_number" class="form-control" placeholder="Account Number" name="account_number" />
+													</div>
+													<div className="pb-2 pl-2 pr-2">
+														<p className="text-left account_name"></p>
 													</div>
 													<div class="form-group">
-														<label for="donationinput2" class="sr-only">Last Name</label>
-														<input type="text" id="donationinput2" class="form-control" placeholder="Last Name" name="lanme" />
+														<label for="transfer_amount" class="sr-only">Last Name</label>
+														<input type="number" id="transfer_amount" class="form-control" placeholder="Enter Amount" name="transfer_amount" disabled/>
 													</div>
 													<div class="form-group">
-														<label for="donationinput3" class="sr-only">E-mail</label>
-														<input type="email" id="donationinput3" class="form-control" placeholder="E-mail" name="email" />
-													</div>
-
-													<div class="form-group">
-														<label for="donationinput4" class="sr-only">Contact Number</label>
-														<input type="text" id="donationinput4" class="form-control" placeholder="Phone" name="phone" />
-													</div>
-													<div class="form-group">
-														<label for="donationinput7" class="sr-only">Message</label>
-														<textarea id="donationinput7" rows="5" class="form-control square" name="message" placeholder="message" ></textarea>
+														<label for="transfer_message" class="sr-only">Message</label>
+														<textarea id="transfer_message" rows="5" class="form-control square" name="transfer_message" placeholder="Message" disabled></textarea>
 													</div>
 													<div class="form-actions center">
-														<button type="submit" class="btn btn-outline-primary">Send</button>
+														<button type="submit" class="btn btn-outline-primary transfer-button" disabled>Send</button>
 													</div>
 												</div>
 											</form>
