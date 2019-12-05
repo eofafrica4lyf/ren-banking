a4beb5e2adf5ca66ae3fc845ce3bdd5d503ecd82
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-12-05 06:39:07 +0100

    add react router; add redirect(auth)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,164 @@
 import React from 'react'
-// import logo from './logo.svg'
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+	useHistory,
+	useLocation
+} from "react-router-dom";
+
 import './App.css'
 import Footer from "./components/Footer"
-import Header from "./components/Header"
+// import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
 import Dashboard from "./components/pages/Dashboard"
 
 function App() {
 	return (
-
 		<div className='App'>
-			<Header />
-			<Sidebar />
+			<Router>
 
-			<Dashboard />
-
-			<Footer />
+				<Switch>
+					<Route path="/public">
+						<PublicPage />
+					</Route>
+					<Route path="/login">
+						<LoginPage />
+					</Route>
+					<PrivateRoute exact path="/">
+						<nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-semi-light">
+							<div class="navbar-wrapper">
+								<div class="navbar-container content">
+									<div class="collapse navbar-collapse show" id="navbar-mobile">
+										<ul class="nav navbar-nav mr-auto float-left">
+											<li class="nav-item d-block d-md-none"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="/#"><i class="ft-menu"></i></a></li>
+											{/* <li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="/#"><i class="ficon ft-maximize"></i></a></li>
+									<li class="nav-item dropdown navbar-search"><a class="nav-link dropdown-toggle hide" data-toggle="dropdown" href="/#"><i class="ficon ft-search"></i></a>
+										<ul class="dropdown-menu">
+											<li class="arrow_box">
+												<form>
+													<div class="input-group search-box">
+														<div class="position-relative has-icon-right full-width">
+															<input class="form-control" id="search" type="text" placeholder="Search here..." />
+															<div class="form-control-position navbar-search-close"><i class="ft-x">   </i></div>
+														</div>
+													</div>
+												</form>
+											</li>
+										</ul>
+									</li> */}
+										</ul>
+										<ul class="nav navbar-nav float-right">
+										</ul>
+										<ul class="nav navbar-nav float-right">
+											<li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="/#" data-toggle="dropdown"><i class="ficon ft-mail">             </i></a>
+												<div class="dropdown-menu dropdown-menu-right">
+													<div class="arrow_box_right"><a class="dropdown-item" href="/#"><i class="ft-book"></i> Read Mail</a><a class="dropdown-item" href="/#"><i class="ft-bookmark"></i> Read Later</a><a class="dropdown-item" href="/#"><i class="ft-check-square"></i> Mark all Read       </a></div>
+												</div>
+											</li>
+											<AuthButton />
+										</ul>
+									</div>
+								</div>
+							</div>
+						</nav>
+						<Sidebar />
+						<Dashboard />
+						<Footer />
+					</PrivateRoute>
+				</Switch>
+			</Router>
 		</div >
 	)
 }
+
+const fakeAuth = {
+	isAuthenticated: false,
+	authenticate(cb) {
+		fakeAuth.isAuthenticated = true;
+		setTimeout(cb, 100); // fake async
+	},
+	signout(cb) {
+		fakeAuth.isAuthenticated = false;
+		setTimeout(cb, 100);
+	}
+};
+
+function AuthButton() {
+	let history = useHistory();
+
+	return fakeAuth.isAuthenticated ? (
+		<>
+			<li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="/#" data-toggle="dropdown">             <span class="avatar avatar-online"><img src="theme-assets/images/portrait/small/avatar-s-19.png" alt="avatar" /><i></i></span></a>
+				<div class="dropdown-menu dropdown-menu-right">
+					<div class="arrow_box_right"><a class="dropdown-item" href="/#"><span class="avatar avatar-online"><img src="theme-assets/images/portrait/small/avatar-s-19.png" alt="avatar" /><span class="user-name text-bold-700 ml-1">Emmanuel</span></span></a>
+						<div class="dropdown-divider"></div><a class="dropdown-item" href="/#"><i class="ft-user"></i> Edit Profile</a><a class="dropdown-item" href="/#"><i class="ft-mail"></i> My Inbox</a><a class="dropdown-item" href="/#"><i class="ft-check-square"></i> Task</a><a class="dropdown-item" href="/#"><i class="ft-message-square"></i> Chats</a>
+						{/* <div class="dropdown-divider"></div><a class="dropdown-item" href="/#"><i class="ft-power"></i> Logout</a> */}
+						<div class="dropdown-divider"></div><a class="dropdown-item" href="/#"><i class="ft-power"></i>
+							<button
+								onClick={() => {
+									fakeAuth.signout(() => history.push("/"));
+								}}
+								style={{ border: "none" }}
+							>
+								Log out
+      				</button>
+						</a>
+					</div>
+				</div>
+			</li>
+		</>
+
+	) : (
+			<p style={{ padding: "1.7rem 1rem 1.6rem 1rem" }}>You are not logged in.</p>
+		);
+}
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+	return (
+		<Route
+			{...rest}
+			render={({ location }) =>
+				fakeAuth.isAuthenticated ? (
+					children
+				) : (
+						<Redirect
+							to={{
+								pathname: "/login",
+								state: { from: location }
+							}}
+						/>
+					)
+			}
+		/>
+	);
+}
+
+function PublicPage() {
+	return <h3>Public</h3>;
+}
+
+function LoginPage() {
+	let history = useHistory();
+	let location = useLocation();
+
+	let { from } = location.state || { from: { pathname: "/" } };
+	let login = () => {
+		fakeAuth.authenticate(() => {
+			history.replace(from);
+		});
+	};
+
+	return (
+		<div>
+			<p>You must log in to view the page at {from.pathname}</p>
+			<button onClick={login}>Log in</button>
+		</div>
+	);
+}
+
 
 export default App
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,14 +5,14 @@ function Sidebar() {
 		<div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="theme-assets/images/backgrounds/02.jpg">
 			<div class="navbar-header">
 				<ul class="nav navbar-nav flex-row">
-					<li class="nav-item mr-auto"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="Chameleon admin logo" src="theme-assets/images/logo/logo.png" />
+					<li class="nav-item mr-auto"><a class="navbar-brand" href="/"><img class="brand-logo" alt="Chameleon admin logo" src="theme-assets/images/logo/logo.png" />
 						<h3 class="brand-text">Ren-Banking</h3></a></li>
 					<li class="nav-item d-md-none"><a class="nav-link close-navbar" href="/#"><i class="ft-x"></i></a></li>
 				</ul>
 			</div>
 			<div class="main-menu-content">
 				<ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-					<li class="active"><a href="index.html"><i class="ft-home"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
+					<li class="active"><a href="/"><i class="ft-home"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
 					</li>
 					<li class=" nav-item"><a href="charts.html"><i class="ft-pie-chart"></i><span class="menu-title" data-i18n="">Charts</span></a>
 					</li>
